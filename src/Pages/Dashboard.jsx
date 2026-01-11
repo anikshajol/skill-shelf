88281aaa389e25d89ab34fbc0a14421689b34b73
anikshajol <anikshajol@gmail.com>
2026-01-12 02:03:40 +0600

    dashboard update as simple

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../Context/Context";
 
 const Dashboard = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user);
+  const lastLogin = new Date(user.metadata.lastSignInTime).toLocaleDateString();
+
   return (
-    <div>
-      <h2>Dashboard</h2>
+    <div className="w-1/2 mx-auto text-center">
+      <h2 className="text-2xl font-bold">{user.displayName}</h2>
+      <p>{user.email}</p>
+      <p>Last Login: {lastLogin}</p>
     </div>
   );
 };
