c3309f09e9402f15a141c4654ec52e7ab249db1c
anikshajol <anikshajol@gmail.com>
2026-01-12 02:13:33 +0600

    dashboard complete as normal

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,15 +2,22 @@ import React, { useContext } from "react";
 import { AuthContext } from "../Context/Context";
 
 const Dashboard = () => {
-  const { user } = useContext(AuthContext);
-  console.log(user);
-  const lastLogin = new Date(user.metadata.lastSignInTime).toLocaleDateString();
+  const { user, saveSkill } = useContext(AuthContext);
+  // console.log(user);
+  const lastLogin = new Date(
+    user?.metadata?.lastSignInTime
+  ).toLocaleDateString();
+  const creationDate = new Date(
+    user?.metadata?.creationTime
+  ).toLocaleDateString();
 
   return (
     <div className="w-1/2 mx-auto text-center">
       <h2 className="text-2xl font-bold">{user.displayName}</h2>
       <p>{user.email}</p>
       <p>Last Login: {lastLogin}</p>
+      <p>Created At: {creationDate}</p>
+      <p>Save Skills: {saveSkill.length}</p>
     </div>
   );
 };
